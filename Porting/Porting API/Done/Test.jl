7be81d2c58e1f249d1cfec2894303df10e389670
DavideFavaro <874161@stud.unive.it>
2021-09-16 17:46:34 +0200

    Download file XML multipli

--- Porting/Porting API/Done/Test.jl
+++ Porting/Porting API/Done/Test.jl
@@ -3,6 +3,7 @@ module Test
 using HTTP
 using Downloads
 
+using CombinedParsers
 
 
 """
@@ -17,11 +18,10 @@ function authenticate( username::AbstractString, password::AbstractString, type:
 end
 
 
-out = "D:\\Vario\\Stage\\data.zip"
-out2 = "C:\\Users\\DAVIDE-FAVARO\\Desktop\\data"
-out3 = "C:\\Users\\DAVIDE-FAVARO\\Desktop\\XML"
-auth = authenticate("davidefavaro","Tirocinio") 
-address = "https://scihub.copernicus.eu/dhus"
+out = [ "D:\\Vario\\Stage",
+        "C:\\Users\\Lenovo\\Desktop\\XML",
+        "C:\\Users\\DAVIDE-FAVARO\\Desktop",
+        "C:\\Users\\DAVIDE-FAVARO\\Desktop\\XML" ]
 
 
 
@@ -56,7 +56,7 @@ function getData( fileId::AbstractString, targetDirectory::AbstractString, authT
                         verbose = true ) #Più info
 end
 
-#getData( "68fc21ec-d9e1-44f1-be45-f487031423a1", "C:\\Users\\DAVIDE-FAVARO\\Desktop", authenticate("davidefavaro", "Tirocinio")  )
+#getData( "68fc21ec-d9e1-44f1-be45-f487031423a1", out[3], authenticate("davidefavaro", "Tirocinio")  )
 
 
 
@@ -70,39 +70,50 @@ end
 #||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
 
-# https://scihub.copernicus.eu/dhus/search?start=0&rows=1&q=ingestiondate:
-#   [NOW-6MONTHS%20TO%20NOW]%20AND%20footprint:%22Intersects(POLYGON((9.5000%2047.0000,%2014.0000%2047.0000,%2014.0000%2044.0000,%209.5000%2044.0000,%209.5000%2047.0000)))%22
+"""
+    getProducts( targetDirectory::AbstractString, authToken::AbstractString[, maxNumber::Union{Integer, Nothing} ] )
 
+Obtain "maxNumber" pages, eachone containing the XML description of 100 products, through "authToken" and store them in "targetDirectory"
 
-#PER IL MOMENTO RITORNA SOLO IL TOTAL COUNT, INOLTRE DEVO TESTARE QUNATO E' VELOCE COMBINEDPERSER RISPETTO A COME HO FATTO IO
-
-function getProducts( targetDirectory::AbstractString, authToken::AbstractString, maxNumber::Integer )
-    start = 0
-    rows = 0
-    address = "https://scihub.copernicus.eu/dhus"
+#Arguments
+- `targetDirectory::AbstractString`: path to the directory of destination
+- `authToken::AbstractString`: Authentication token, obtained through "authenticate()" function
+- `maxNumber::Integer`: maximum number of pages to download
+"""
+function getProducts( targetDirectory::AbstractString, authToken::AbstractString, maxNumber::Union{Integer, Nothing} = nothing )
+# Definition of the components of The URL
     aoi = "POLYGON((9.5000%2047.0000,%2014.0000%2047.0000,%2014.0000%2044.0000,%209.5000%2044.0000,%209.5000%2047.0000))"
     query2 = "[NOW-6MONTHS%20TO%20NOW]%20AND%20footprint:\"Intersects($aoi)\""
-    query = "/search?start=$start&rows=$rows&q=ingestiondate:$query2"
+    query = "search?start=0&rows=0&q=ingestiondate:$query2"
+    0file = targetDirectory*"\\0.xml"
 
-    # Get number of total products 
-    #Downloads.download( "$address$query",
-    #                    targetDirectory*"\\data.xml",
-    #                    headers = [ "Authorization" => "Basic $(authenticate("davidefavaro","Tirocinio"))" ], #info di autorizzazioone
-    #                    progress = ( total, now ) -> println("$(now/total*100)% ( $now / $total )"), #Funzione che permette di controllare lo stato del download
-    #                    verbose = true ) #Più info
-    Downloads.download( "$address$query", targetDirectory*"\\data.xml", headers = [ "Authorization" => "Basic $(authenticate("davidefavaro","Tirocinio"))" ] )
-    lines = readlines( targetDirectory*"\\data.xml" )
+# Get number of total products
+    #Download a short XML file
+    Downloads.download( "https://scihub.copernicus.eu/dhus/$query", 0file, headers = [ "Authorization" => "Basic $authToken" ] )
+    lines = readlines( 0file )
+    #Filter its contents to obtain the total number ofproducts
     count = tryparse( Int64, split( split( filter( line -> occursin( "totalResults", line ), lines )[1], ">" )[2], "<" )[1] )
+    #After retrieving the total count, remove the XML file
+    rm( 0file )
 
-    # DA AGGIUNGERE LA PARTE CHE ITERA SU TUTTI GLI XML 
 
-    return count
+# Get the XML files of the existing products
+    #Check if maxNumber has an actual value, if so use it to limit the number of collected pages
+        #One page holds the XML specifications of 100 products
+    if !isnothing(maxNumber) && maxNumber > 0
+        if maxNumber < count
+            count = maxNumber*100
+        end
+    end
+    #Get the desired number of pages
+    for i in range( 0, count, step=100 )
+        query = "search?start=$i&rows=100&q=ingestiondate:$query2"
+        Downloads.download( "https://scihub.copernicus.eu/dhus/$query", targetDirectory*"\\$((i ÷ 100) + 1).xml", headers = [ "Authorization" => "Basic $authToken" ] )
+    end
 end
 
-    
 
-
-getProducts( out3, authenticate("davidefavaro","Tirocinio"), 10 )
+getProducts( out[2], authenticate("davidefavaro","Tirocinio"), 200 )
                         
                         
 
