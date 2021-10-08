5076ea0450cf43345d14d6a5b174d341386397f7
DavideFavaro <71487859+DavideFavaro@users.noreply.github.com>
2021-10-08 13:16:04 +0200

    Implementazione aggiornamento dati memorizzati

--- Code/ProductsData.jl
+++ Code/ProductsData.jl
@@ -111,13 +111,12 @@ end
 #                                                            DOWNLOAD DEI FILE XML COLLEGATI AI PRODOTTI
 #||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-
 """
-    getProductsBuffer( authToken::AbstractString[, maxNumber::Union{Integer, Nothing} ] )
+    getProductsBuffer( authToken::AbstractString[, start::Integer, maxNumber::Union{Integer, Nothing} ] )
 
-Obtain "maxNumber" XML description of products, through "authToken", returning the IOBuffer that contains them all
+Obtain `maxNumber` XML description of products, starting from `start`, through `authToken`, returning the IOBuffer that contains them all
 """
-function getProductsBuffer( authToken::AbstractString, maxNumber::Union{Integer, Nothing} = nothing )
+function getProductsBuffer( authToken::AbstractString; start::Integer=0, max::Union{Integer, Nothing} = nothing, last::Bool=false )
 # Definition of the components of The URL
     aoi = "POLYGON((9.5000%2047.0000,%2014.0000%2047.0000,%2014.0000%2044.0000,%209.5000%2044.0000,%209.5000%2047.0000))"
     query2 = "[NOW-6MONTHS%20TO%20NOW]%20AND%20footprint:\"Intersects($aoi)\""
@@ -130,23 +129,34 @@ function getProductsBuffer( authToken::AbstractString, maxNumber::Union{Integer,
     lines = String(take!(iob))
     val = tryparse( Int64, split( split( lines, "totalResults>" )[2], "<" )[1] )
     # "count" has two values: the first is the number of pages (undreds of products) and the second is a number of products smaller than 100 to be downloaded in an additional page
-    count = [ val ÷ 100, val % 100 ]
+    count = val
 
-# Check if maxNumber has a sensible value
-    if !isnothing( maxNumber ) && maxNumber > 0 && maxNumber < val
-        count[1] = maxNumber ÷ 100
-        count[2] = maxNumber % 100
+    #Check if `start` has a sensible value
+    if ( last && start == 0 ) || start > val
+        start = val -1
     end
 
-# Download the desired number pages and sore the in an IOBuffer
+    # Check if `maxNumber` has a sensible value
+    if !isnothing( max ) && max > 0 && max < val
+        count = max
+    end
+
+    # Calculate the value of count in terms of pages of 100 products each accounting for the starting point
+    if start < count
+        count -= start
+    end
+    count = [ count ÷ 100, count % 100 ]
+
+
+# Download the desired number pages and store the in an IOBuffer
     if count[1] > 0
         for i in range( 0, count[1] - 1, step=1 )
-            query = "search?start=$(i*100)&rows=100&q=ingestiondate:$query2"
+            query = "search?start=$(start + ( i * 100 ))&rows=100&q=ingestiondate:$query2"
             Downloads.download( "https://scihub.copernicus.eu/dhus/$query", iob, headers = [ "Authorization" => "Basic $authToken" ] )
         end
     end
     if count[2] > 0
-        query = "search?start=$(count[1] * 100)&rows=$(count[2])&q=ingestiondate:$query2"
+        query = "search?start=$(start + (count[1] * 100))&rows=$(count[2])&q=ingestiondate:$query2"
         Downloads.download( "https://scihub.copernicus.eu/dhus/$query", iob, headers = [ "Authorization" => "Basic $authToken" ] )
     end
     return iob
@@ -164,25 +174,26 @@ end
 
 
 
-
-
-
 """
     getProductsDicts( fileIO::IO )
 
 Given the IOBuffer obtained through "getProductsBuffer()", return an array of the dictionaries containing the informations on each of the products of the page 
 """
 function getProductsDicts( fileIO::IO )
-# Get the downloaded XML representations of the products
+    # Get the downloaded XML representations of the products
     original = replace( String( take!(fileIO) ), "\n" => "" )
-# Split the downloaded files into pages
+
+    # Split the downloaded files into pages
     pages = split( original, r"<\?xml [^<>]+><[^<>]+>", keepempty=false )
-# Split the result in a vector of ready-to-be-parsed strings representing single products
+
+    # Split the result in a vector of ready-to-be-parsed strings representing single products
     vector = reduce( vcat, [ split( page, r"</?entry>", keepempty=false )[2:end-1] for page in pages ] )
-# Parse the strings
+
+    # Parse the strings
     products = [ product(x)[8:end] for x in vector ]
-# Generate a vector of dictionaries containing the details for each product of the original page adding to each of them an additional value to account for the
-    # original order of the data
+
+    # Generate a vector of dictionaries containing the details for each product of the original page adding to each of them an additional value to account for the
+        # original order of the data
     return [setindex!(
                 Dict( Symbol( join( prod[:opening][7] ) ) => parseConvert( join( prod[:type][1] ), join( prod[:content] ) ) for prod in products[i] ),
                 i,
@@ -193,21 +204,22 @@ end
 
 #   dict = getProductsDicts(io)
 
-#io = getProductsBuffer( authenticate("davidefavaro","Tirocinio"), 300 )
-#res = getProductsDicts(io)
+#   io = getProductsBuffer( authenticate("davidefavaro","Tirocinio"), 5115, 50 )
+#   res = getProductsDicts(io)
 
 
 
 
 
 """
-    getProductsDF( authToken::AbstractString, maxNumber::Union{Integer, Nothing} = nothing )
+    getProductsDF( authToken::AbstractString[, start::Integer, maxNumber::Union{Integer, Nothing} ] )
 
-Generate the DataFrame containing the data of "maxNumber" products using "authToken", if "maxNumber" is not specified, the df will include all available products 
+Generate the DataFrame containing the data of `maxNumber` products using `authToken`, if `maxNumber` is not specified, the df will include all available products,
+if `start` is specified the downloaded data will begin from that index
 """
-function getProductsDF( authToken::AbstractString, maxNumber::Union{Integer, Nothing} = nothing )
+function getProductsDF( authToken::AbstractString; start::Integer = 0, max::Union{Integer, Nothing} = nothing, last::Bool=false )
 # Download "maxNumber" pages and return the buffer containing them
-    io = getProductsBuffer(  authToken, maxNumber )
+    io = getProductsBuffer( authToken, start = start, max = max, last = last )
 # Create a vector of dictionaries of the products
     dict_vect = getProductsDicts( io )
 # Obtain the existing subsets of attributes of the products
@@ -263,23 +275,16 @@ function saveProductsDF( targetDirectory::AbstractString, data::DataFrame; overw
     if condition
         # Preexisting data
         old_data = CSV.read( targetDirectory*"\\data.csv", DataFrame )
-        
-# DA TESTARE QUANDO CI SARANNO EFFETTIVAMENTE DATI NUOVI
-        # Index of the last element of the preexisting data
-            # It will be used to find the duplicated data in "data"
-        old_last_id = odata[end, :uuid]
-        #Index of the first element of the new data
-            # It will be used to find the now-unavailable products in "old_data"
-        new_first_id = data[1, :uuid]
 
-        # Index of the last duplicate in "data"
-        duplicated_index = findfirst( ==(old_last_id), data[:, :uuid] )
-        # Index of the last available product in "old_data"
-        unavailable_index = findfirst( ==(new_first_id), old_data )
+        # Index of the first entry of the preexisting data
+            # It will be used to find the duplicates in "data"
+        old_first_id = old_data[1, :uuid]
 
-        #filter!( prod -> rownumber(prod) > duplicated_index, data )
-        data = data[1:duplicated_index, :]
-        old_data[ 1:unavailable_index, :available ] = false
+        # Index of the first duplicate in "data"
+        first_dup_idx = findfirst( ==(old_first_id), data[:, :uuid] )
+
+        # Remove all the duplicates from "data"
+        filter!( prod -> rownumber(prod) > first_dup_idx, data )
     end
     CSV.write( targetDirectory*"\\data.csv", data, append = condition )
 end
@@ -320,17 +325,57 @@ end
 
 
 
+function checkAvailabile()
+    # Get the last available product
+    data = getProductsDF( authenticate("davidefavaro","Tirocinio"), max=1, last=true  )
+
+    old_data = CSV.read( *( @__DIR__, "\\Dati di prova\\data.csv"), DataFrame )
+
+    last_available_id = data[end, :uuid]
+
+    # Index of the last available product in "old_data"
+    first_unav_idx = findfirst( ==(last_available_id), old_data[:, :uuid] )
+    map!( x -> !x, old_data[first_unav_idx:end, :available ] ) 
+end
+
+
 
 #df = getProductsDF( authenticate("davidefavaro","Tirocinio"), 1000 )
 #saveProductsDF( out[2], df )
 
 
 
-"C:\\Users\\Lenovo\\Documents\\GitHub\\Tirocinio\\Dati di prova"
+df = getProductsDF( authenticate("davidefavaro","Tirocinio"), max=1, last=true )
+odf = CSV.read( "D:\\Documents and Settings\\DAVIDE-FAVARO\\My Documents\\GitHub\\Tirocinio\\Dati di prova\\data.csv", DataFrame )
 
-df = getProductsDF( authenticate("davidefavaro","Tirocinio") )
-saveProductsDF( "C:\\Users\\Lenovo\\Documents\\GitHub\\Tirocinio\\Dati di prova", df )
 
+
+
+
+
+old_last_id = odf[end, :uuid]
+new_first_id = ndf[1, :uuid]
+
+duplicated_index = findfirst( prod_id ->  prod_id == old_last_id, df[:, :uuid] )
+unavailable_index = findfirst( prod_id -> prod_id == new_first_id, odf[:, :uuid] )
+
+#filter!( prod -> rownumber(prod) > duplicated_index, data )
+data = data[1:duplicated_index, :]
+old_data[ 1:unavailable_index, :available ] = false
+
+
+
+
+old_first_id = odf[1, :uuid]
+new_last_id = df[end, :uuid]
+
+duplicated_begin_index = findfirst( ==(old_first_id), df[:, :uuid] )
+unavailable_index = findfirst( ==(new_last_id), odf[:, :uuid] )
+
+
+
+#saveProductsDF( "C:\\Users\\Lenovo\\Documents\\GitHub\\Tirocinio\\Dati di prova", df )
+saveProductsDF( "D:\\Documents and Settings\\DAVIDE-FAVARO\\My Documents\\GitHub\\Tirocinio\\Dati di prova", df )
 
 
 
--- Test/GetProductsPages-Indexing.jl
+++ Test/GetProductsPages-Indexing.jl
@@ -18,3 +18,44 @@ function indexing( count )
         println( "...\\$(count[1]+1).xml" )
     end
 end
+
+
+
+function indexing2( start::Integer, val::Integer, maxNumber::Union{Nothing, Integer}=nothing )
+    count = val
+    println( "COUNT : ", count )
+
+#Check if `start` has a sensible value
+    if start > val
+        start = val - 1
+    end
+    println( "START : ",start)
+
+# Check if `maxNumber` has a sensible value
+    if !isnothing( maxNumber ) && maxNumber > 0 && maxNumber < val
+        count = maxNumber
+    end
+
+    if count > start
+        count -= start
+    end
+
+    println( "COUNT(U1) : ", count )
+    count = [ count ÷ 100, count % 100 ]
+    println( "MAXNUMBER : ", maxNumber )
+    println( "COUNT(U2) : ", count )
+    println()
+
+    if count[1] > 0
+        println("count[1] > 0")
+        for i in range( 0, count[1] - 1, step=1 )
+            println( "start=$( start + ( i * 100 ) ) & rows=100" )
+        end
+    end
+    if count[2] > 0
+        println("count[2] > 0")
+        println( "start=$(count[1] * 100 + start) & rows=$(count[2])" )
+    end
+    println()
+    println("THE END")
+end
