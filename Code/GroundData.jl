e4627a7c63620bf9dac1941e61b3de9691b63b1c
DavideFavaro <874161@stud.unive.it>
2021-10-12 16:37:57 +0200

    Parsing dati singole stazioni
    Non ancora del tutto funzionante

--- Code/GroundData.jl
+++ Code/GroundData.jl
@@ -35,7 +35,8 @@ using Revise
                                      )
                        )
 
-@syntax stations = Repeat(
+
+@syntax stations = Repeat( 
                         Sequence(
                             re"<[^<>]+>",
                             Either(
@@ -53,6 +54,36 @@ using Revise
                     )
 
 
+@syntax sensor = Sequence(
+                    Repeat( # Sensor info
+                        re"<(?!D)[^>]+>",
+                        Either(
+                            Numeric(Int),
+                            re"<!\[CDATA\[[^<>]+\]\]>",
+                            re"[^>]+"
+                        ),
+                        re"</[^>]+>"
+                    ),
+                    Repeat( # DATI + Repeat(V.+)
+                          Sequence( "<DATI ISTANTE=\"", Numeric(Int64), "\">" ),
+                          Repeat( # V.+ ( VM, VMIN, VMAX )
+                              Sequence( # V.+
+                                  re"<V[^<>]+>",
+                                  Numeric(Float64),
+                                  re"<[^>]+>"
+                              )
+                          ),
+                          "</DATI>"
+                    ) 
+                  )
+
+
+
+"""
+    getStationsInfo()::DataFrames.DataFrame
+
+Obtain the dataframe containing informations on all available ARPAV stations 
+"""
 function getStationsInfo()
 
 #Get the information on the available stations
@@ -65,15 +96,16 @@ function getStationsInfo()
     # Split the downloaded string in substrings one for each station
     arr = split( useful, r"</?STAZIONE>", keepempty=false )
 
-    info = informations(arr[1])
+    #info = informations(arr[1])
     stats = stations.(arr[2:end])
 
+    #Create the dictionary of the stations, checking the type of `attribute[2]` (contains the value of the attribute)
     dict = [ Dict( Symbol( lowercase( String( attribute[1][2] ) ) )
              =>
-             isa(attribute[2], Number) ?
+             isa(attribute[2], Number) ? # If it's a number leave it as is
                 attribute[2] :
-                isa(attribute[2], Array) ?
-                    lowercase( String( attribute[2] ) ) :
+                isa(attribute[2], Array) ? #If it is an array (of strings) convert it to String 
+                    lowercase( String( attribute[2] ) ) : # If it is a Tuple ( Es. `("<![CDATA[", "Arabba", "]]>")` ) take the actual value ( `"Arabba"` ) 
                     titlecase( String( attribute[2][2] ) ) for attribute in station ) for station in stats ]
 
     df = DataFrame(dict)
@@ -82,7 +114,56 @@ function getStationsInfo()
 end
 
 
+"""
+    getStationsData( stat::AbstractString )
+
+Obtain the dataframe containing the data of a single station described by `stat`
+"""
+# INCOMPLETO
+function getStationsData( stat::AbstractString )
+    page = String( HTTP.get("https://www.arpa.veneto.it/bollettini/meteo60gg/$stat").body )
+    stat_string = split( page, "</COMUNE>" )[2][1:end-26]
+    sensors_string_vect = split( stat_string, r"</?SENSORE>", keepempty=false )
+    stat_data = sensor.(sensors_string_vect)
+
+    return stat_data
+end
+
+
+"""
+    getStationsData( stats::AbstractVector{String} )
+
+Obtain the dataframe containing the data of all the stations described by the elements of `stats`
+"""
+# INCOMPLETO
+function getStationsData( stats::AbstractVector{String} )
+    pages_vect = [ String( HTTP.get("https://www.arpa.veneto.it/bollettini/meteo60gg/$stat").body ) for stat in stats ]
+
+    # For each of the station remove the first part of the string and the closing tags
+    stat_strings_vect = [ split(page, "</ATTIVAZIONE>")[2][1:end-26] for page in pages_vect ]
+
+    # From each station generate the corresponding array of sensors
+    sensor_vect = split.( stat_strings_vect, r"</?SENSORE>", keepempty=false )
+
+    # vect[i][j] i-th station, j-th sensor
+    # vect[i][j][1] j-th sensor's info (Vector)
+    # vect[i][j][2] j-th sensor's data (Vector)
+    # vect[i][j][2][l] j-th sensor'data, l-th measurement (Vector)
+    # vect[i][j][2][l][4] l-th measurement's values (Vector 1 => mean, 2 => min, 3 => max)
+    vect = [ sensor.(sensor_group) for sensor_group in sensor_vect ]
+
+    return vect
+end
+
+
+
+
 df = getStationsInfo()
+vect = getStationsData( df[1:3, :linkstaz] )
+
+
+
+
 
 
 end # moduled
